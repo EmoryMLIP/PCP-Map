2aec5a05b64bfb518fa44db8a54bbd3d91387f9a
olivrw <oliver.wang@emory.edu>
2024-01-24 15:16:56 -0500

    update lv sampling script

--- sample_stoch_lv.py
+++ sample_stoch_lv.py
@@ -1,12 +1,10 @@
-import numpy as np
 import scipy.io
 from datasets.StochasticLotkaVolterra import StochasticLotkaVolterra
 
 num_samples = 50000
 stoch = StochasticLotkaVolterra()
-x_test, y_test = stoch.sample_joint(num_samples)
-data = np.concatenate((x_test, y_test), axis=1)
+x_train, y_train = stoch.sample_joint(num_samples)
 
 # TODO specify file path
 file_name = '.../PCP-Map/datasets/lv_data.mat'
-scipy.io.savemat(file_name, {'data': data})
+scipy.io.savemat(file_name, {'x_train': x_train, 'y_train': y_train})
