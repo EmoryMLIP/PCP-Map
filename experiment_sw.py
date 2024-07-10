d362b66e2125345b8e6ca90123b88202d384aad5
Zheyu Oliver Wang <109133106+OliverWang7@users.noreply.github.com>
2024-07-10 10:33:16 -0400

    Update experiment_sw.py

--- experiment_sw.py
+++ experiment_sw.py
@@ -3,8 +3,8 @@ import numpy as np
 import pandas as pd
 
 # TODO change to correct paths
-loss = pd.read_csv('.../PCP-Map/experiments/cond/sw_valid_hist.csv').to_numpy()
-param = pd.read_csv('.../PCP-Map/experiments/cond/sw_params_hist.csv').to_numpy()
+loss = pd.read_csv('.../PCP-Map/experiments/tabcond/sw_valid_hist.csv').to_numpy()
+param = pd.read_csv('.../PCP-Map/experiments/tabcond/sw_params_hist.csv').to_numpy()
 loss_param = np.concatenate((param[:, 1:], loss[:, 1:]), axis=1)
 unique_param = loss_param[np.unique(loss_param[:, :-1], return_index=True, axis=0)[1]]
 unique_param = unique_param[unique_param[:, -1].argsort()]
